cab0e7aeccbafbb9fd54c2981e7e358551f1b560
541711153@qq.com <541711153@qq.com>
2025-05-24 13:54:22 +0800

    feat: 分片详情界面开发完毕

--- elastic-service-provider/src/main/java/com/dipper/monitor/entity/elastic/shard/ShardMigrationReq.java
+++ elastic-service-provider/src/main/java/com/dipper/monitor/entity/elastic/shard/ShardMigrationReq.java
@@ -1,53 +1,14 @@
 package com.dipper.monitor.entity.elastic.shard;
 
 
+import lombok.Data;
+
+@Data
 public class ShardMigrationReq {
     private String index;
     private Integer shard;
     private String fromNode;
     private String toNode;
+    private String priority;
 
-    public String getIndex() {
-        return index;
-    }
-
-    public void setIndex(String index) {
-        this.index = index;
-    }
-
-    public Integer getShard() {
-        return shard;
-    }
-
-    public void setShard(Integer shard) {
-        this.shard = shard;
-    }
-
-    public String getFromNode() {
-        return fromNode;
-    }
-
-    public void setFromNode(String fromNode) {
-        this.fromNode = fromNode;
-    }
-
-    public String getToNode() {
-        return toNode;
-    }
-
-    public void setToNode(String toNode) {
-        this.toNode = toNode;
-    }
-
-    // 如果需要添加isPrimary字段
-    private Boolean isPrimary;
-    
-    // 并添加对应的getter和setter
-    public Boolean getIsPrimary() {
-        return isPrimary;
-    }
-    
-    public void setIsPrimary(Boolean isPrimary) {
-        this.isPrimary = isPrimary;
-    }
 }
